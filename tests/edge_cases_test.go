471f1204c6884b1374ef1483565a1345cd0c654a
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2025-12-03 16:21:20 +0100

    test: remove case sensitive edge case test
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- tests/edge_cases_test.go
+++ tests/edge_cases_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"path/filepath"
-	"runtime"
 )
 
 func (s *FremenTestSuite) Test_EdgeCase_MixedLockfiles() {
@@ -17,15 +16,4 @@ func (s *FremenTestSuite) Test_EdgeCase_NoLockfiles() {
 	report, exitCode := s.runFremenJSON(target)
 	s.Equal(1, exitCode)
 	s.Equal(0, report.Summary.TotalProjects)
-}
-
-func (s *FremenTestSuite) Test_EdgeCase_CaseSensitivity() {
-	target := filepath.Join(s.fixturesDir, "cases", "edges", "case_sensitivity")
-	report, _ := s.runFremenJSON(target, "--recursive")
-
-	if runtime.GOOS == "darwin" || runtime.GOOS == "windows" {
-		s.Equal(0, report.Summary.InfectedProjects, "Should NOT find infected projects in Node_Modules on case-insensitive OS")
-	} else {
-		s.GreaterOrEqual(report.Summary.InfectedProjects, 1, "Should find infected projects in Node_Modules on case-sensitive OS")
-	}
 }
